36c3dee95e64ae1f2b073082db14514c86533c72
JE-FUNES <infoventacba@gmail.com>
2025-03-18 22:40:32 -0300

    by Julia Funes

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// By Julia Funes
 const textInput = document.getElementById("text-input");
 const checkButton = document.getElementById("check-btn");
 const result = document.getElementById("result");
